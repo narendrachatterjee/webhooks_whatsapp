257604f89d37660b5110c82ead4735a76e0b1df8
Narendra <narendra.chatterjee@gmail.com>
2024-09-04 17:54:07 +0530

    commit

--- utils/user_info.js
+++ utils/user_info.js
@@ -1,9 +1,11 @@
 export let user_phone_number;
 export let user_phone_number_id;
 
-export const userInfo = (req) =>{
-    
-    user_phone_number_id = req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
-    user_phone_number = req.body.entry?.[0].changes?.[0].value?.statuses?.[0]?.recipient_id;
-    console.log(user_phone_number, "   ",user_phone_number_id )
-}
+export const userInfo = (req) => {
+  user_phone_number_id =
+    req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+  const contact = req.body.entry[0].changes[0].value.contacts[0];
+  const profileName = contact.profile.name;
+  const user_phone_number = contact.wa_id;
+  console.log(user_phone_number, "   ", user_phone_number_id);
+};
